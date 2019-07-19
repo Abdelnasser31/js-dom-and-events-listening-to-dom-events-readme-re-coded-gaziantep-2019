baae034b1862ae2643a66f00f98f233b18eb181f
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-07-19 10:41:59 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,2 +1,6 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+const main = document.querySelector("#main");
+main.addEventListner("click",function () { 
+  alert("I was clicked");
+})
